89cd6e496a5bfe5f0094feef59d881db0cb54ef0
bltproof <aka.vincente.vega@gmail.com>
2020-12-14 11:25:34 +0300

    added security support

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,10 +3,12 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -57,5 +59,20 @@ public class UserController {
     public String updateUser(@ModelAttribute("user") User user) {
         userService.update(user);
         return "redirect:/";
+    }
+
+    @GetMapping(value = "hello")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
+    }
+
+    @GetMapping(value = "login")
+    public String loginPage() {
+        return "login";
     }
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,10 +1,15 @@
 package web.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,12 +21,23 @@ public class User {
     @Column()
     private byte age;
 
+    private String password;
+
+    private Set<Role> roles;
+
     public User() {
     }
 
     public User(String name, byte age) {
         this.name = name;
         this.age = age;
+    }
+
+    public User(Long id, String name, String password, Set<Role> roles) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.roles = roles;
     }
 
     public String getName() {
@@ -46,5 +62,52 @@ public class User {
 
     public void setAge(byte age) {
         this.age = age;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
